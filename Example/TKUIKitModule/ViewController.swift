e37c88a3e5877ff81a7a7190e9a5e007716533ff
聂子 <1021491936@qq.com>
2019-02-27 23:09:17 +0800

    add test code

--- Example/TKUIKitModule/ViewController.swift
+++ Example/TKUIKitModule/ViewController.swift
@@ -7,18 +7,40 @@
 //
 
 import UIKit
+import TKUIKitModule
 
 class ViewController: UIViewController {
 
+    private var dataSource: [String] = []
+    private var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView = UITableView.init(frame: view.bounds, style: .plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
+extension ViewController : UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellId = "dequeueReusableCell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellId)
+        if cell == nil {
+            cell = UITableViewCell.init(style: .default, reuseIdentifier: cellId)
+        }
+        return cell!
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
