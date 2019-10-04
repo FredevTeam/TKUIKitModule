f82c0f3348ea109e1be6c710bb112589594d1a7d
zhuamaodeyu <playtomandjerry@gmail.com>
2019-10-04 23:47:02 +0800

    no message

--- TKUIKitModule/Classes/Extension/UIButton+Ext.swift
+++ TKUIKitModule/Classes/Extension/UIButton+Ext.swift
@@ -7,27 +7,90 @@
 
 import Foundation
 
-fileprivate var kActionBlock = "UIButtonActionBlock"
+
 extension UIButton {
+    private struct AssociatedBlockKey {
+        static var kActionBlock = "UIButtonActionBlock"
+    }
     var block:((_ sender:UIButton?) -> Void)?
     {
         get {
-            return (objc_getAssociatedObject(self, &kActionBlock) as? (UIButton?) -> Void)
+            return (objc_getAssociatedObject(self, &AssociatedBlockKey.kActionBlock) as? (UIButton?) -> Void)
         }
         set(newValue) {
-            objc_setAssociatedObject(self, &kActionBlock, newValue, .OBJC_ASSOCIATION_COPY)
+            objc_setAssociatedObject(self, &AssociatedBlockKey.kActionBlock, newValue, .OBJC_ASSOCIATION_COPY)
         }
     }
     @objc func buttonAction() {
-        if !self.isEnabled {
-            return
+        if self.canEvent {
+            self.canEvent = false
+            self.block?(self)
+            self.perform(#selector(updateCanEvent), with: NSNumber.init(value: true), afterDelay: self.eventInterval)
         }
-        self.isEnabled = false
-        self.block?(self)
-        self.isEnabled = true
     }
 }
 
+// MARK: - UIButton EventInterval
+extension UIButton {
+    private struct AssociatedIntervalKey {
+        static var timerIntervalKey = "timerIntervalKey"
+        static var canEventKey = "canEventKey"
+        static var methodSwizzingKey = "methodSwizzingKey"
+    }
+
+    /// 事件间隔
+    public var eventInterval: TimeInterval {
+        get {
+            return (objc_getAssociatedObject(self, &AssociatedIntervalKey.timerIntervalKey) as? TimeInterval) ?? 0.0
+        }
+        set(newValue) {
+            objc_setAssociatedObject(self, &AssociatedIntervalKey.timerIntervalKey, newValue, .OBJC_ASSOCIATION_ASSIGN)
+            if !UIButton.methodSwizzing {
+                methodSwizzingAction()
+            }
+        }
+    }
+    @objc var canEvent: Bool {
+        get {
+            return (objc_getAssociatedObject(self, &AssociatedIntervalKey.canEventKey) as? Bool) ?? true
+        }
+        set(newValue) {
+            objc_setAssociatedObject(self, &AssociatedIntervalKey.canEventKey, newValue, .OBJC_ASSOCIATION_ASSIGN)
+        }
+    }
+    @objc static var methodSwizzing: Bool {
+        get {
+            return (objc_getAssociatedObject(self, &AssociatedIntervalKey.methodSwizzingKey) as? Bool) ?? false
+        }
+        set(newValue) {
+            objc_setAssociatedObject(self, &AssociatedIntervalKey.methodSwizzingKey, newValue, .OBJC_ASSOCIATION_ASSIGN)
+        }
+    }
+
+    @objc func tk_sendAction(_ action: Selector, to target: Any?, for event: UIEvent?) {
+        if self.canEvent {
+            self.canEvent = false
+            self.tk_sendAction(action, to: target, for: event)
+            self.perform(#selector(updateCanEvent), with: NSNumber.init(value: true), afterDelay: self.eventInterval)
+        }
+    }
+    @objc private func updateCanEvent() {
+        self.canEvent = true
+    }
+
+    private func methodSwizzingAction() {
+        DispatchQueue.once(UUID.init().uuidString + "Button_TimeInterval") {
+            // 方法交换
+            UIView.swizzling(UIButton.self, originalSelector: #selector(sendAction(_:to:for:)), swizzledSelector: #selector(tk_sendAction(_:to:for:)))
+            UIButton.methodSwizzing = true
+        }
+    }
+}
+
+
+
+
+// MARK: - UIButton create
 extension TypeWrapperProtocol where WrappedType == UIButton {
     /// 通过 title 创建button
     ///
